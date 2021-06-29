dbf4eb883696e45a88075968a6451f1cde1250a6
jus1d <artfa63@yandex.ru>
2021-06-30 02:31:50 +0400

    Add bot

---
+++ jus1dBot/Bot.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace jus1dBot
+{
+    public class Bot
+    {
+        public DiscordClient client { get; private set; }
+        public InteractivityExtension interactivity { get; private set; }
+        public CommandsNextExtension commands { get; private set; }
+        
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+            
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+            
+            client = new DiscordClient(config);
+
+            var activity = "new DiscordActivity();";
+            
+            
+            client.Ready += OnClientReady;
+            client.UpdateStatusAsync();
+            client.UseInteractivity(new InteractivityConfiguration
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableDms = false,
+                EnableMentionPrefix = true,
+                DmHelp = true,
+            };
+
+            commands = client.UseCommandsNext(commandsConfig);
+            commands.RegisterCommands<Commands>();
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(DiscordClient c, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ jus1dBot/Commands/Commands.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+using Microsoft.VisualBasic;
+
+namespace jus1dBot
+{
+    public class Commands : BaseCommandModule
+    {
+        //pinging
+        [Command("ping")]
+        [Description("returns pong")]
+        [RequireRoles(RoleCheckMode.All, "admin")]
+        public async Task Ping(CommandContext msg)
+        {
+            msg.Channel.SendMessageAsync("pong");
+        }
+
+        //response
+        [Command("response")]
+        [RequireRoles(RoleCheckMode.All, "admin")]
+        public async Task Resonse(CommandContext msg)
+        {
+            var interactivity = msg.Client.GetInteractivity();
+
+            var message = await interactivity.WaitForMessageAsync(x => x.Channel == msg.Channel).ConfigureAwait(false);
+
+            await msg.Channel.SendMessageAsync(message.Result.Content);
+        }
+
+        // -channelid
+        [Command("channelid")]
+        [Description("Returns current channel ID")]
+        public async Task ChannelID(CommandContext msg)
+        {
+            if(msg.Channel.Name != "bot-commands")
+                return;
+            
+            msg.Channel.SendMessageAsync(msg.Channel.Id.ToString()).ConfigureAwait(false);
+        }
+        [Command("channelid")]
+        [Description("Returns current channel ID")]
+        public async Task ChannelID(CommandContext msg, DiscordChannel channel)
+        {
+            if(msg.Channel.Name != "bot-commands")
+                return;
+            
+            msg.Channel.SendMessageAsync($"{channel.Mention} channel ID: {channel.Id}").ConfigureAwait(false);
+        }
+        [Command("channelid")]
+        [Description("Returns current channel ID")]
+        public async Task ChannelID(CommandContext msg, params string[] parametres)
+        {
+            if(msg.Channel.Name != "bot-commands")
+                return;
+
+            var templateEmbed = new DiscordEmbedBuilder
+            {
+                Title = "Template -channelid:",
+                Description = "-channelid <channel>",
+                Color = DiscordColor.Azure
+                
+            };
+            
+            msg.Channel.SendMessageAsync(templateEmbed).ConfigureAwait(false);
+        }
+        
+        // -invitelink
+        [Command("invitelink")]
+        public async Task InviteLink(CommandContext msg)
+        {
+            var message = msg.Channel.SendMessageAsync($"Here your link, {msg.User.Mention}\n " +
+                                                       $"https://discord.com/api/oauth2/authorize?client_id=849009875031687208&permissions=8&scope=bot");
+        }
+        
+        // -userinfo
+        [Command("userinfo")]
+        public async Task UserInfo(CommandContext msg)
+        {
+            var user = msg.User;
+            
+            string userCreatedDate = "";
+            
+            for (int i = 0; i < user.CreationTimestamp.ToString().Length - 7; i++)
+            {
+                userCreatedDate = userCreatedDate + user.CreationTimestamp.ToString()[i];
+            }
+
+            msg.Channel.SendMessageAsync($"{user.Mention}'s Info:\n" +
+                                         $"User ID: {user.Id}\n" +
+                                         $"Date account created: {userCreatedDate}\n" +
+                                         $"User's avatar URL: {user.AvatarUrl}");
+        }
+        [Command("userinfo")]
+        public async Task UserInfo(CommandContext msg, DiscordMember user)
+        {
+            string userCreatedDate = "";
+            
+            for (int i = 0; i < user.CreationTimestamp.ToString().Length - 7; i++)
+            {
+                userCreatedDate = userCreatedDate + user.CreationTimestamp.ToString()[i];
+            }
+            
+            msg.Channel.SendMessageAsync($"{user.Mention}'s Info:\n" +
+                                         $"User ID: {user.Id}\n" +
+                                         $"Date account created: {userCreatedDate}\n" +
+                                         $"User's avatar URL: {user.AvatarUrl}");
+        }
+        
+        // -useravatar <@user>
+        [Command("useravatar")]
+        [RequireRoles(RoleCheckMode.All, "admin")]
+        public async Task UserAvatar(CommandContext msg, DiscordMember user)
+        {
+            msg.Channel.SendMessageAsync($"{user.Mention}'s avatar URL: {user.AvatarUrl}");
+        }
+    }
+}
