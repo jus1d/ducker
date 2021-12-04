be33a17aeb78e81a73bb9525a39b00469377c90b
jus1d <artfa63@gmail.com>
2021-12-04 13:27:28 +0400

    refactor grantedEmbed in event handler

--- duckerBot/EventHandler.cs
+++ duckerBot/EventHandler.cs
@@ -81,9 +81,9 @@ namespace duckerBot
         public static async Task OnComponentInteractionCreated(DiscordClient client, InteractionCreateEventArgs e)
         {
             DiscordMember member = (DiscordMember) e.Interaction.User;
+            DiscordEmbedBuilder grantedEmbed;
             if (e.Interaction.Data.CustomId == "get_follow_role")
             {
-                var grantedEmbed = new DiscordEmbedBuilder();
                 if (member.Roles.Contains(e.Interaction.Guild.GetRole(Role.TwitchFollowerRoleId)))
                 {
                     await member.RevokeRoleAsync(e.Interaction.Guild.GetRole(Role.TwitchFollowerRoleId));
@@ -118,7 +118,6 @@ namespace duckerBot
             }
             else if (e.Interaction.Data.CustomId == "get_chel_role")
             {
-                var grantedEmbed = new DiscordEmbedBuilder();
                 if (member.Roles.Contains(e.Interaction.Guild.GetRole(Role.ChelRoleId)))
                 {
                     await member.RevokeRoleAsync(e.Interaction.Guild.GetRole(Role.ChelRoleId));
