e37725cfc8e2612435e47b03adf94e0dc75761b0
jus1d <artfa63@yandex.ru>
2021-12-12 03:33:21 +0400

    add database class

---
+++ ducker/Database.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace ducker;
+
+public class Database
+{
+    private MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=ducker");
+
+    public void OpenConnection()
+    {
+        if (connection.State == ConnectionState.Closed)
+        {
+            connection.Open();
+        }
+    }
+
+    public void CloseConnection()
+    {
+        if (connection.State == ConnectionState.Open)
+        {
+            connection.Close();
+        }
+    }
+
+    public MySqlConnection GetConnection()
+    {
+        return connection;
+    }
+}
