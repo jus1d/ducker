2bbbe61cc1c8c3595886142f0b3f4bd27f9ffabb
jus1d <artfa63@yandex.ru>
2021-12-12 13:30:04 +0400

    add test command for db

--- ducker/Commands/DBCommands.cs
+++ ducker/Commands/DBCommands.cs
@@ -1,24 +1,66 @@
 ﻿using System.Data;
+using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 using MySqlConnector;
 
 namespace ducker;
 
 public partial class Commands
 {
-    [Command("login")]
-    public async Task LogIn(CommandContext msg, string login, string password)
+    [Command("set-music-channel"), Aliases("smc"), RequirePermissions(Permissions.Administrator)]
+    public async Task LogIn(CommandContext msg, DiscordChannel channel)
+    {
+        Database database = new Database();
+        DataTable table = new DataTable();
+        DataTable findGuildTable = new DataTable();
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+        MySqlCommand findGuildCommand = new MySqlCommand($"SELECT * FROM `main` WHERE `guildId` = '{msg.Guild.Id}'", database.GetConnection());
+        adapter.SelectCommand = findGuildCommand;
+        adapter.Fill(findGuildTable);
+        if (findGuildTable.Rows.Count > 0)
+        {
+            MySqlCommand command = new MySqlCommand($"UPDATE `main` SET `musicChannelId` = @musicChannelId WHERE `main`.`guildId` = @guildId",
+                database.GetConnection());
+            command.Parameters.Add("@guildId", MySqlDbType.VarChar).Value = msg.Channel.Guild.Id;
+            command.Parameters.Add("@musicChannelId", MySqlDbType.VarChar).Value = channel.Id;
+            
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+        }
+        else
+        {
+            MySqlCommand command = new MySqlCommand("INSERT INTO `main` (`guildId`, `musicChannelId`) VALUES (@guildId, @musicChannelId)", 
+                database.GetConnection());
+            command.Parameters.Add("@guildId", MySqlDbType.VarChar).Value = msg.Channel.Guild.Id;
+            command.Parameters.Add("@musicChannelId", MySqlDbType.VarChar).Value = channel.Id;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+        }
+    }
+
+    [Command("t")]
+    public async Task Test(CommandContext msg)
     {
         Database database = new Database();
         DataTable table = new DataTable();
         MySqlDataAdapter adapter = new MySqlDataAdapter();
-        
-        MySqlCommand command = new MySqlCommand("INSERT INTO `users` (`id`, `login`, `pass`) VALUES (NULL, @ul, @up)", database.GetConnection());
-        command.Parameters.Add("@ul", MySqlDbType.VarChar).Value = login;
-        command.Parameters.Add("@up", MySqlDbType.VarChar).Value = password;
 
+        MySqlCommand command = new MySqlCommand($"SELECT `musicChannelId` FROM `main` WHERE `guildId` = {msg.Guild.Id}", database.GetConnection());
         adapter.SelectCommand = command;
         adapter.Fill(table);
+        ulong musicChannelIdFromDB = 0;
+        if (table.Rows.Count > 0)
+            musicChannelIdFromDB = ulong.Parse(table.Rows[0].ItemArray[0].ToString());
+
+        await msg.Channel.SendMessageAsync(musicChannelIdFromDB.ToString());
+
+        if (msg.Channel.Id != Bot.MusicChannelId && msg.Channel.Id != musicChannelIdFromDB)
+        {
+            await msg.Channel.SendMessageAsync("not music channel");
+        }
     }
 }
