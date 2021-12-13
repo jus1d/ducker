e5aca22933dfefc2e3da24be863bf415c4c8b362
jus1d <artfa63@yandex.ru>
2021-12-14 00:36:51 +0400

    reqrite mute command

--- ducker/Commands/AdminCommands.cs
+++ ducker/Commands/AdminCommands.cs
@@ -485,12 +485,46 @@ namespace ducker
          RequirePermissions(Permissions.Administrator)]
         public async Task Mute(CommandContext msg, DiscordMember member)
         {
-            await member.GrantRoleAsync(msg.Guild.GetRole(Role.MutedRoleId));
+            ulong muteRoleId = Database.GetMuteRoleId(msg.Guild.Id);
+            if (muteRoleId == 0)
+            {
+                DiscordRole muteRole = await msg.Guild.CreateRoleAsync("Muted", Permissions.None, DiscordColor.DarkGray, false, false);
+
+                var channels = await msg.Guild.GetChannelsAsync();
+
+                foreach (var channel in channels)
+                {
+                    await channel.AddOverwriteAsync(muteRole, Permissions.None, Permissions.SendMessages);
+                }
+
+                muteRoleId = muteRole.Id;
+
+                Database database = new Database();
+                DataTable table = new DataTable();
+                DataTable findGuildTable = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+                MySqlCommand findGuildCommand = new MySqlCommand($"SELECT * FROM `ducker` WHERE `guildId` = '{msg.Guild.Id}'", 
+                    database.GetConnection());
+                adapter.SelectCommand = findGuildCommand;
+                adapter.Fill(findGuildTable);
+                
+                MySqlCommand command = new MySqlCommand($"UPDATE `ducker` SET `muteRoleId` = {muteRoleId} WHERE `ducker`.`guildId` = {msg.Guild.Id}",
+                    database.GetConnection());
+            
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+                await member.GrantRoleAsync(msg.Guild.GetRole(Database.GetMuteRoleId(msg.Guild.Id)));
+            }
+            else
+            {
+                await member.GrantRoleAsync(msg.Guild.GetRole(Database.GetMuteRoleId(msg.Guild.Id)));
+            }
         }
         
         // -mute
         [Command("mute"), RequirePermissions(Permissions.Administrator)]
-        public async Task Mute(CommandContext msg, params string[] txt)
+        public async Task Mute(CommandContext msg, [RemainingText] string txt)
         {
             var incorrectCommandEmbed = new DiscordEmbedBuilder
             {
