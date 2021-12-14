649f721dcaeaf3569cb3bddeb6ad0f0cc25b725f
jus1d <artfa63@yandex.ru>
2021-12-14 23:41:15 +0400

    add summary to role class

--- ducker/DiscordData/Role.cs
+++ ducker/DiscordData/Role.cs
@@ -2,8 +2,13 @@
 {
     public class Role
     {
-        public static ulong MutedRoleId = 911479294209970187;
+        /// <summary>
+        /// Get chel role (only for main bot guild)
+        /// </summary>
         public static ulong ChelRoleId = 816666984745140254;
+        /// <summary>
+        /// Get Twitch Follower role (only for main bot guild)
+        /// </summary>
         public static ulong TwitchFollowerRoleId = 914921577634754600;
     }
 }
