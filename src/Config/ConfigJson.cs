6f0e332c0b090cb446bad26298fd8678d4fe92d1
jus1d <artfa63@yandex.ru>
2021-12-15 15:10:18 +0400

    add summary to ConfigJson

--- src/Config/ConfigJson.cs
+++ src/Config/ConfigJson.cs
@@ -5,21 +5,40 @@ namespace ducker.Config
 {
     public class ConfigJson
     {
+        /// <summary>
+        /// Get bot token from config file
+        /// </summary>
         [JsonProperty("token")]
         public string Token { get; private set; }
         
+        /// <summary>
+        /// Get bot commands prefix from config file
+        /// </summary>
         [JsonProperty("prefix")]
         public string Prefix { get; private set; }
         
+        /// <summary>
+        /// Get bot ID from config file
+        /// </summary>
         [JsonProperty("id")]
         public ulong Id { get; private set; }
         
+        /// <summary>
+        /// Get Spotify Client ID from config file
+        /// </summary>
         [JsonProperty("spotifyId")]
         public string SpotifyId { get; private set; }
         
+        /// <summary>
+        /// Get Spotify Client Secret from config file
+        /// </summary>
         [JsonProperty("spotifySecret")]
         public string SpotifySecret { get; private set; }
 
+        /// <summary>
+        /// Method to get config field
+        /// </summary>
+        /// <returns>Return config field</returns>
         public static ConfigJson GetConfigField()
         {
             var json = string.Empty;
