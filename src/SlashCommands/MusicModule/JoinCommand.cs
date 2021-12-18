5e36661c5d6613449f74ee374417cdedc21f883a
jus1d <artfa63@yandex.ru>
2021-12-19 02:12:00 +0400

    /join

---
+++ src/SlashCommands/MusicModule/JoinCommand.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.Lavalink;
+using DSharpPlus.SlashCommands;
+using ducker.Database;
+
+namespace ducker.SlashCommands.MusicModule
+{
+    public partial class MusicSlashCommands : ApplicationCommandModule
+    {
+        [SlashCommand("join", "Join your voice channel")]
+        public async Task JoinCommand(InteractionContext msg)
+        {
+            await msg.CreateResponseAsync(DiscordEmoji.FromName(msg.Client, Bot.RespondEmojiName));
+            ulong musicChannelIdFromDb = DB.GetMusicChannel(msg.Guild.Id);
+            ulong cmdChannelIdFromDb = DB.GetCmdChannel(msg.Guild.Id);
+            
+            if (musicChannelIdFromDb == 0)
+            {
+                await msg.Channel.SendMessageAsync(Embed.NoMusicChannelConfigured(msg.User));
+                return;
+            }
+            if (msg.Channel.Id != musicChannelIdFromDb && msg.Channel.Id != cmdChannelIdFromDb)
+            {
+                await msg.Channel.SendMessageAsync(Embed.IncorrectMusicChannelEmbed(msg, musicChannelIdFromDb));
+                return;
+            }
+            if (msg.Member.VoiceState == null || msg.Member.VoiceState.Channel == null)
+            {
+                await msg.Channel.SendMessageAsync(Embed.NotInVoiceChannelEmbed(msg));
+                return;
+            }
+            var lava = msg.Client.GetLavalink();
+            var node = lava.ConnectedNodes.Values.First();
+            await node.ConnectAsync(msg.Member.VoiceState.Channel);
+        }
+    }
+}
