fa3907157a19c8cc971e16d0a09dd0a054fc4cb0
jus1d <artfa63@yandex.ru>
2021-12-18 14:09:23 +0400

    remove todo, update

--- src/Commands/AdministrationModule/ClearCommand.cs
+++ src/Commands/AdministrationModule/ClearCommand.cs
@@ -38,8 +38,8 @@ namespace ducker.Commands.AdministrationModule
                 else
                     messageOrMessages = "messages";
                 
-                await Log.LogToAudit(msg.Guild, $"{msg.Member.Mention} deleted {amount} {messageOrMessages} in {msg.Channel.Mention}");
-                // TODO: logs(@jus1d cleared amount messages from #channel, message list:)
+                await Log.LogToAudit(msg.Guild, $"{msg.Member.Mention} cleared {amount} {messageOrMessages} in {msg.Channel.Mention}");
+                // TODO: logs(@jus1d cleared amount messages from #channel, message list)
                 
                 DiscordMessage message = await msg.Channel.SendMessageAsync(new DiscordEmbedBuilder
                 {
