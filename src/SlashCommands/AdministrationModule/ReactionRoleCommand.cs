5ba09ccf9b611ccc71aadc580963f6f45a004ab5
jus1d <artfa63@yandex.ru>
2021-12-19 01:56:53 +0400

    add /rr

---
+++ src/SlashCommands/AdministrationModule/ReactionRoleCommand.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using ducker.Attributes;
+
+namespace ducker.SlashCommands.AdministrationModule
+{
+    public partial class AdministrationSlashCommands
+    {
+        [SlashCommand("reaction-role-embed", "Send embed with reactions, press them to get role"),
+         RequireAdmin]
+        public async Task ReactionRoleEmbed(InteractionContext msg)
+        {
+            DiscordEmoji twitchRgbEmoji = DiscordEmoji.FromName(msg.Client, ":twitchrgb:");
+            DiscordEmoji chelEmoji = DiscordEmoji.FromName(msg.Client, ":chel:");
+            var followButton = new DiscordButtonComponent(ButtonStyle.Secondary, "get_follow_role", "", false, new DiscordComponentEmoji(twitchRgbEmoji));
+            var chelButton = new DiscordButtonComponent(ButtonStyle.Secondary, "get_chel_role", "", false, new DiscordComponentEmoji(chelEmoji));
+            
+            await msg.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder()
+                    .AddEmbed(ducker.Embed.ReactionRolesEmbed(msg.Client, msg.Guild))
+                    .AddComponents(followButton, chelButton));
+        }
+    }
+}
