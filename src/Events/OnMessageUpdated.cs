721fc45f3b2d5fb9924ff7a355d2a9de17248789
jus1d <artfa63@yandex.ru>
2021-12-18 14:01:47 +0400

    add exception catch

--- src/Events/OnMessageUpdated.cs
+++ src/Events/OnMessageUpdated.cs
@@ -8,7 +8,14 @@ namespace ducker.Events
     {
         public static async Task OnMessageUpdated(DiscordClient client, MessageUpdateEventArgs e)
         {
-            await Log.LogToAudit(e.Guild,$"{e.Author.Mention} changed the message: `{e.MessageBefore.Content}` -> `{e.Message.Content}` in {e.Channel.Mention}");
+            try
+            {
+                await Log.LogToAudit(e.Guild,$"{e.Author.Mention} changed the message: `{e.MessageBefore.Content}` -> `{e.Message.Content}` in {e.Channel.Mention}");
+            }
+            catch
+            {
+                await Log.LogToAudit(e.Guild, $"{e.Author.Mention} changed message with {e.Message.Id} ID. `I can't load message before content (message not cached)` -> `{e.Message.Content}` in {e.Channel.Mention}");
+            }
         }
     }
 }
