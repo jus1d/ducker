87837134de6f66dc1b87fcc7c613c1acaf811eef
jus1d <artfa63@yandex.ru>
2021-12-19 13:07:59 +0400

    add require main guild check

---
+++ src/SlashCommands/Attributes/RequireMainGuild.cs
@@ -0,0 +1,12 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace ducker.SlashCommands.Attributes
+{
+    public class RequireMainGuild : SlashCheckBaseAttribute
+    {
+        public override async Task<bool> ExecuteChecksAsync(InteractionContext msg)
+        {
+            return msg.Guild.Id == Bot.MainGuildId;
+        }
+    }
+}
