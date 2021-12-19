d30eda9cd85cb96366c67c24bc9d9ddf6134dd51
jus1d <artfa63@yandex.ru>
2021-12-19 13:08:28 +0400

    add summary

--- src/SlashCommands/Attributes/RequireAdmin.cs
+++ src/SlashCommands/Attributes/RequireAdmin.cs
@@ -3,6 +3,9 @@ using DSharpPlus.SlashCommands;
 
 namespace ducker.SlashCommands.Attributes
 {
+    /// <summary>
+    /// Check whether the command invoker has admin permissions at current server
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class RequireAdmin : SlashCheckBaseAttribute
     {
