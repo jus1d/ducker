5ca8f01af0d8701732a5c077ba1285e5a4488e3c
jus1d <artfa63@yandex.ru>
2021-12-19 21:41:21 +0400

    add message respond

--- src/Commands/Attributes/RequireMainGuild.cs
+++ src/Commands/Attributes/RequireMainGuild.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 
 namespace ducker.Commands.Attributes
 {
@@ -11,6 +12,19 @@ namespace ducker.Commands.Attributes
     {
         public override async Task<bool> ExecuteCheckAsync(CommandContext msg, bool help)
         {
+            if (msg.Guild.Id != Bot.MainGuildId)
+            {
+                await msg.Channel.SendMessageAsync(new DiscordEmbedBuilder
+                {
+                    Description = $"This command can be used only in `{(await msg.Client.GetGuildAsync(Bot.MainGuildId)).Name}` discord server",
+                    Footer = new DiscordEmbedBuilder.EmbedFooter
+                    {
+                        IconUrl = msg.User.AvatarUrl,
+                        Text = msg.User.Username
+                    },
+                    Color = Bot.MainEmbedColor
+                });
+            }
             return msg.Guild.Id == Bot.MainGuildId;
         }
 
--- src/SlashCommands/Attributes/RequireMainGuild.cs
+++ src/SlashCommands/Attributes/RequireMainGuild.cs
@@ -1,4 +1,5 @@
-﻿using DSharpPlus.SlashCommands;
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
 
 namespace ducker.SlashCommands.Attributes
 {
@@ -10,6 +11,19 @@ namespace ducker.SlashCommands.Attributes
     {
         public override async Task<bool> ExecuteChecksAsync(InteractionContext msg)
         {
+            if (msg.Guild.Id != Bot.MainGuildId)
+            {
+                await msg.Channel.SendMessageAsync(new DiscordEmbedBuilder
+                {
+                    Description = $"This command can be used only in `{(await msg.Client.GetGuildAsync(Bot.MainGuildId)).Name}` discord server",
+                    Footer = new DiscordEmbedBuilder.EmbedFooter
+                    {
+                        IconUrl = msg.User.AvatarUrl,
+                        Text = msg.User.Username
+                    },
+                    Color = Bot.MainEmbedColor
+                });
+            }
             return msg.Guild.Id == Bot.MainGuildId;
         }
     }
