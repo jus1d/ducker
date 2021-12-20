88a62384ad404a28467a0c7c2f2188e474ee971f
jus1d <artfa63@yandex.ru>
2021-12-20 23:49:17 +0400

    Rewrite DB.Get()/Update() (#50)

--- src/Database/DB.cs
+++ src/Database/DB.cs
@@ -136,7 +136,30 @@ public class DB
         adapter.Fill(table);
     }
 
-    public static string Get(ulong guildId, string field)
+    public static ulong GetId(ulong guildId, string field)
+    {
+        DB db = new DB();
+        DataTable table = new DataTable();
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+        db.OpenConnection();
+        MySqlCommand command = new MySqlCommand($"SELECT `{field}` FROM `ducker` WHERE `guildId` = {guildId}", db.GetConnection());
+        adapter.SelectCommand = command;
+        adapter.Fill(table);
+        if (table.Rows.Count > 0)
+        {
+            try
+            {
+                return ulong.Parse(table.Rows[0].ItemArray[0].ToString());
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+        return 0;
+    }
+    
+    public static string GetString(ulong guildId, string field)
     {
         DB db = new DB();
         DataTable table = new DataTable();
@@ -153,9 +176,9 @@ public class DB
             }
             catch
             {
-                return "Data not found";
+                return "No data found";
             }
         }
-        return "Data not found";
+        return "No data found";
     }
 }
