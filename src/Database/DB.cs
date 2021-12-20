3dd84a2c68b48f130d6b230aff7bf7f24eee0ba8
jus1d <artfa63@yandex.ru>
2021-12-20 23:39:42 +0400

    Database method for add/update fields (#48)

--- src/Database/DB.cs
+++ src/Database/DB.cs
@@ -124,4 +124,38 @@ public class DB
         }
         return 0;
     }
+
+    public static void Update(ulong guildId, string field, string value)
+    {
+        DB db = new DB();
+        DataTable table = new DataTable();
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+        db.OpenConnection();
+        MySqlCommand command = new MySqlCommand($"UPDATE `ducker` SET `{field}` = {value} WHERE `ducker`.`guildId` = {guildId}", db.GetConnection());
+        adapter.SelectCommand = command;
+        adapter.Fill(table);
+    }
+
+    public static string Get(ulong guildId, string field)
+    {
+        DB db = new DB();
+        DataTable table = new DataTable();
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+        db.OpenConnection();
+        MySqlCommand command = new MySqlCommand($"SELECT `{field}` FROM `ducker` WHERE `guildId` = {guildId}", db.GetConnection());
+        adapter.SelectCommand = command;
+        adapter.Fill(table);
+        if (table.Rows.Count > 0)
+        {
+            try
+            {
+                return table.Rows[0].ItemArray[0].ToString();
+            }
+            catch
+            {
+                return "Data not found";
+            }
+        }
+        return "Data not found";
+    }
 }
