9d2ed73f303254c6abd543674e57989de3b5e9be
jus1d <artfa63@yandex.ru>
2021-12-28 17:01:50 +0400

    add /tempban (#85)

---
+++ src/SlashCommands/AdministrationModule/TempbanCommand.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using ducker.Logs;
+using ducker.SlashCommands.Attributes;
+
+namespace ducker.SlashCommands.AdministrationModule
+{
+    public partial class AdministrationSlashCommands
+    {
+        [SlashCommand("tempban", "Temporarily ban mentioned member"), RequireAdmin]
+        public async Task TempbanCommand(InteractionContext msg, 
+            [Option("member", "Member to ban")] DiscordUser user, 
+            [Option("duration", "Ban duration in hours")] long duration,
+            [Option("reason", "Reason for ban")] string reason = "No reason given")
+        {
+            await msg.CreateResponseAsync(DiscordEmoji.FromName(msg.Client, Bot.RespondEmojiName));
+            DiscordMember member = (DiscordMember)user;
+            await member.BanAsync(0, reason);
+            await Log.Audit(msg.Guild, $"{msg.User.Mention} banned {user.Mention} for {duration} minutes", reason);
+            Thread.Sleep((int)duration * 60000 * 60);
+            await member.UnbanAsync(msg.Guild, reason);
+            await Log.Audit(msg.Guild, $"{user.Mention} unbanned", "Ban time expired");
+        }
+    }
+}
