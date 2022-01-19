a607ab3de83306d662cd2cfdaeccfe7833e65f9f
jus1d <artfa63@yandex.ru>
2022-01-19 21:11:11 +0400

    fix /about

--- src/SlashCommands/MiscModule/AboutCommand.cs
+++ src/SlashCommands/MiscModule/AboutCommand.cs
@@ -7,9 +7,9 @@ namespace ducker.SlashCommands.MiscModule;
 public partial class MiscSlashCommands
 {
     [SlashCommand("about", "Send information about mentioned member or you")]
-    public async Task AboutCommand(InteractionContext msg, DiscordUser user = null)
+    public async Task AboutCommand(InteractionContext msg, DiscordUser user)
     {
-        DiscordMember member = (DiscordMember) user ?? msg.Member;
+        DiscordMember member = (DiscordMember) user;
         await msg.CreateResponseAsync(DiscordEmoji.FromName(msg.Client, Bot.RespondEmojiName));
         await msg.Channel.SendMessageAsync(Embed.AboutMemberEmbed(member));
     }
